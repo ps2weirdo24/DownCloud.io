615751f16cd65f24cfbe298e08ea6119198a71b9
ryangaudy <ryangaudy@yahoo.com>
2015-10-26 06:14:50 -0700

    Added an error screen
    Added some exception handling to the main file and added an error screen
    if the user entered an invalid URL.

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -40,33 +40,36 @@ def processurl():
 
     target_url = request.args.get("soundcloudurl")
     client = soundcloud.Client(client_id=MYID)
-    res_list = client.get("/resolve", url=target_url)
+    try:
+        res_list = client.get("/resolve", url=target_url)
+    except:
+        return render_template("playerror.html", wrong_url=target_url)
+    else:
+        pl_name = (res_list.title).encode("utf-8")
+        pl_artwork = res_list.artwork_url
+        pl_user = (res_list.user["username"]).encode("utf-8")
+        pl_number_of_tracks = str(len(res_list.tracks))
+        folder_name = "mysite/downloads/" + pl_name + " - " + pl_user
+        safe_folder_name = "downloads/" + pl_name + " - " + pl_user
+        os.mkdir(folder_name)
+        track_count = 1
+        good_count = 0
+        for track in res_list.tracks:
+            if track["streamable"]:
+		        step1_stream_url = track["stream_url"]
+		        track_filename = folder_name + "/" + (track["title"]).encode("utf-8") + ".mp3"
+		        step2_stream_url = client.get(step1_stream_url, allow_redirects=False)
+		        step3_stream_url = step2_stream_url.location
+		        this_dl = wget.download(step3_stream_url, out=track_filename)
+		        good_count += 1
 
-    pl_name = (res_list.title).encode("utf-8")
-    pl_user = (res_list.user["username"]).encode("utf-8")
-    pl_number_of_tracks = str(len(res_list.tracks))
-    folder_name = "mysite/downloads/" + pl_name + " - " + pl_user
-    safe_folder_name = "downloads/" + pl_name + " - " + pl_user
-    os.mkdir(folder_name)
-    track_count = 1
-    good_count = 0
+            track_count += 1
 
-    for track in res_list.tracks:
-        if track["streamable"]:
-		    step1_stream_url = track["stream_url"]
-		    track_filename = folder_name + "/" + (track["title"]).encode("utf-8") + ".mp3"
-		    step2_stream_url = client.get(step1_stream_url, allow_redirects=False)
-		    step3_stream_url = step2_stream_url.location
-		    this_dl = wget.download(step3_stream_url, out=track_filename)
-		    good_count += 1
-
-        track_count += 1
-
-    zipf = zipfile.ZipFile((folder_name + ".zip"), 'w')
-    zipdir(folder_name, zipf)
-    zipf.close()
-    shutil.rmtree(folder_name)
-    return render_template("process.html", channel=pl_user, playlist_name=pl_name, link=(safe_folder_name + ".zip"))
+        zipf = zipfile.ZipFile((folder_name + ".zip"), 'w')
+        zipdir(folder_name, zipf)
+        zipf.close()
+        shutil.rmtree(folder_name)
+        return render_template("process.html", channel=pl_user, artwork_url=pl_artwork, playlist_name=pl_name, link=(safe_folder_name + ".zip"))
 
 
 if __name__ == "__main__":
